e0a7abdf3f20c32ea6ef108fc1f978ce1e1b7973
Azhar Khan <azhar.ak@gmail.com>
2017-03-31 11:19:13 +0530

    Update TestImageIoServlet.java

--- test-imageio-war/src/main/java/test/TestImageIoServlet.java
+++ test-imageio-war/src/main/java/test/TestImageIoServlet.java
@@ -1,1 +1,33 @@
+package test;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestImageIoServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		URL url = TestImageIoServlet.class.getClassLoader().getResource("test.tif");
+		File file = new File(url.getFile());
+		byte[] bytesArray = new byte[(int) file.length()];
+		FileInputStream fis = new FileInputStream(file);
+		fis.read(bytesArray); // read file into bytes[]
+		fis.close();
+
+		BufferedImage image = ImageIO.read(new ByteArrayInputStream(bytesArray));
+
+		response.getWriter().write("<html><body>Image width = " + image.getWidth() + "</body></html>");
+	}
+
+}
