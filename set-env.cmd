75406030e508d51545919427ad2b4a9b19ae6cfd
Azhar Khan <azhar.ak@gmail.com>
2017-03-31 11:14:45 +0530

    Create set-env.cmd

---
+++ set-env.cmd
@@ -0,0 +1,7 @@
+REM set JAVA_HOME=C:\Software\IBM855\WebSphere\AppServer\java_1.8_64
+REM set JAVA_HOME=C:\Software\IBM855\WebSphere\AppServer\java
+set JAVA_HOME=C:\Software\Java\jdk1.7.0_80
+set M2_HOME=C:\Software\apache-maven-3.0.1
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms256m -Xmx1g
+set PATH=%JAVA_HOME%\bin;%M2%;%PATH%
